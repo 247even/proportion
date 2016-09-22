a9a1a95e2af755a12b6a7f1d6b0628a0979fa626
247even <247even@users.noreply.github.com>
2016-09-22 17:17:15 +0200

    Update proportion.js

--- proportion.js
+++ proportion.js
@@ -1,6 +1,6 @@
 function proportion(el,a,b) {
-		var a = !a ? 1 : a;
-		var b = !b ? 1 : b;
+		var a = !a ? 0 : a;
+		var b = !b ? 0 : b;
 		for (i = 0; i < el.length; i++) {
 			el[i].style.height = el[i].offsetWidth * b / a+'px';
 		}	
